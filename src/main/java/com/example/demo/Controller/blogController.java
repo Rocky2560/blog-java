28f8da45e6f1204bccb5ebc51698f8d0282ac889
sudipmanandhar2560@gmail.com <Sudip@2560>
2025-01-06 20:28:01 +0000

    First commit from aws instance

--- src/main/java/com/example/demo/Controller/blogController.java
+++ src/main/java/com/example/demo/Controller/blogController.java
@@ -8,8 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -62,19 +65,32 @@ public class blogController {
         postService.deletePosts(postid);
     }
     @PostMapping("/addPost")
-    public String addPost(@ModelAttribute posts posts) {
+    public String addPost(@RequestParam("title") String title, @RequestParam("description") String description,@RequestParam("category") String category, @RequestParam("image")MultipartFile image, RedirectAttributes redirectAttributes) {
         // Handle the form data
-        postService.saveorUpdate(posts);
+        try
+        {
+            posts posts = new posts();
+            posts.setTitle(title);
+            posts.setDescription(description);
+            posts.setCategory(category);
+//            System.out.println("hello");
+//            posts.setImage(image);
+
+            postService.saveorUpdate(posts, image);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+//        postService.saveorUpdate(posts);
         return "index";
     }
 
 
-    @PutMapping("/posts")
-    public posts update (@RequestBody posts posts)
-    {
-        postService.saveorUpdate(posts);
-        return posts;
-    }
+//    @PutMapping("/posts")
+//    public posts update (@RequestBody posts posts)
+//    {
+//        postService.saveorUpdate(posts);
+//        return posts;
+//    }
 
 
     @GetMapping("editPost/{id}")
@@ -87,9 +103,21 @@ public class blogController {
         return ResponseEntity.ok(post);
     }
     @PostMapping("editPost")
-    public String updateExpense(@ModelAttribute posts posts)
-    {
-        postService.saveorUpdate(posts);
-        return "redirect:/api/postDetails";
+    public String updateExpense(@RequestParam("id") int id, @RequestParam("title") String title, @RequestParam("description")
+                                    String description, @RequestParam("category") String category,
+                                @RequestParam("image1") MultipartFile image,
+                                RedirectAttributes redirectAttributes) throws IOException {
+
+        try {
+            posts posts = postService.getPostById(id);
+            posts.setTitle(title);
+            posts.setDescription(description);
+            posts.setCategory(category);
+            postService.saveorUpdate(posts, image);
+            return "redirect:/api/postDetails";
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
